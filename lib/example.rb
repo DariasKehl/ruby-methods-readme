7423d9d6a958d8e09a6b9664d784cd52e696f2be
DariasKehl <neocount@gmail.com>
2020-10-20 09:12:50 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -3,11 +3,12 @@
 phrase = "Hello World!"
 puts phrase
 puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+puts "\n\n\n"
+
+name = "Morty"
+
+def greeting(name)
+  puts "Hello " + name
+end
+
+greeting(name)
